8797fd53a58bce941209e6dcedad7c649453fd5f
Sivapavankumar <156369837+Sivapavankumar@users.noreply.github.com>
2024-03-27 21:03:32 -0500

    saved all

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # s24wb02chava
+Render Link : https://s24wb02chava.onrender.com
+
+Class name Ball
+
+Ball_name,Ball_brand,cost
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var jRouter = require('./routes/Ball');
+var gRouter = require('./routes/grid');
+var rRouter = require('./routes/pick');
 var app = express();
 
 // view engine setup
@@ -21,7 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/Ball',jRouter);
+app.use('/grid', gRouter);
+app.use('/randomitem', rRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
